705716c12ab081b6063c2e660c3f6c92f6d471d9
Timo-u.de <timo@timo-u.de>
2019-01-06 17:29:10 +0100

    Add errorhandling for invalid Tokens Add missing Parameters

--- Pushover/README.md
+++ Pushover/README.md
@@ -15,27 +15,41 @@ Token um ein Bestimmtes Gerät anzusprechen
 
 #### Nachrichten Senden mit Priorität 0
 ```php
-TUPO_SendMessage(12345,"Title","Message",0
+TUPO_SendMessage(12345/*instanz-ID*/,"Title","Message",0);
 ```
 
 #### Nachrichten mit URL Senden
 ```php
-TUPO_SendMessageUrl(12345,"Title","Message","https://google.de","Link zu google",0);
+TUPO_SendMessageUrl(12345/*instanz-ID*/,"Title","Message","https://google.de","Link zu google",0);
+```
+
+#### Nachrichten im HTML-Format senden
+```php
+SendMessageUrl(12345/*instanz-ID*/,"Title",'<b>Text</b>',"https://google.de","URL-Title",0/* priority */)
+```
+
+
+#### Nachrichten mit allen Parametern senden
+```php
+TUPO_SendMessageComplete(12345/*instanz-ID*/,"Title",'Text',"https://google.de","URL-Title",0/* priority */,0/* content is HTML */,30 /* retry */,60 /* expire */,'pianobar' /* sound */ );
 ```
 
 #### Glances Text senden
 ```php
-TUPO_Glances(23345,"Title","Text","subtext");
+TUPO_Glances(12345/*instanz-ID*/,"Title","Text","subtext");
 ```
 #### Glances Zähler setzen
 ```php
-TUPO_GlancesCount(12345,-49);
+TUPO_GlancesCount(12345/*instanz-ID*/,-49);
 ```
 #### Glances Prozentwert setzen (0 bis 100)
 ```php
-TUPO_GlancesPercent(12345,98);
+TUPO_GlancesPercent(12345/*instanz-ID*/,98);
 ```
 #### Glances Zurücksetzen
 ```php
-TUPO_Glances(23345,"Title","Text","subtext");
+TUPO_Glances(12345/*instanz-ID*/,"Title","Text","subtext");
 ```
+
+
+
--- Pushover/module.php
+++ Pushover/module.php
@@ -19,28 +19,20 @@
 
         public function SendMessage(string $title, string $message, int $priority = 0)
         {
-            curl_setopt_array($ch = curl_init(), [
-            CURLOPT_URL        => 'https://api.pushover.net/1/messages.json',
-            CURLOPT_POSTFIELDS => [
-                    'token'    => $this->ReadPropertyString('ApplicationToken'),
-                    'user'     => $this->ReadPropertyString('UserToken'),
-                    'device'   => $this->ReadPropertyString('DeviceToken'),
-                    'title'    => $title,
-                    'message'  => $message,
-                    'priority' => $priority,
-                    'retry'    => 30,
-                    'expire'   => 120,
-                    ],
-                CURLOPT_SAFE_UPLOAD    => true,
-                CURLOPT_RETURNTRANSFER => true,
-                ]);
-            curl_exec($ch);
-            curl_close($ch);
-
-            return true;
+            return $this -> SendMessageComplete($title,$message,'','',$priority,0,60,3600,'');
         }
 
+		public function SendMessageHtml(string $title, string $message, int $priority = 0)
+        {
+            return $this -> SendMessageComplete($title,$message,'','',$priority,1,60,3600,'');
+        }
+		
         public function SendMessageUrl(string $title, string $message, string $url = '', string $urlTitle = '', int $priority = 0)
+        {
+           return $this -> SendMessageComplete($title,$message,$url,$urlTitle,$priority,0,60,3600,'');
+        }
+		
+		public function SendMessageComplete(string $title, string $message, string $url = '', string $urlTitle = '', int $priority = 0, int $html = 0 , int $retry = 30, int $expire = 3600,string $sound = '')
         {
             curl_setopt_array($ch = curl_init(), [
             CURLOPT_URL        => 'https://api.pushover.net/1/messages.json',
@@ -53,16 +45,34 @@
                     'url'       => $url,
                     'url_title' => $urlTitle,
                     'priority'  => $priority,
-                    'retry'     => '30',
-                    'expire'    => '120',
+					'html'  	=> $html,
+					'sound'  	=> $sound,					
+                    'retry'     => $retry,
+                    'expire'    => $expire
                     ],
                 CURLOPT_SAFE_UPLOAD    => true,
                 CURLOPT_RETURNTRANSFER => true,
                 ]);
-            curl_exec($ch);
+			$response = curl_exec($ch);
             curl_close($ch);
-
-            return true;
+			
+			$responseObject = json_decode($response);
+			if ($responseObject == null) 
+			{
+				return false;
+			}
+			if($responseObject-> {'status'} == 1)
+			{
+				$this->SetStatus(102);
+				return true;
+			}			
+			if(property_exists($responseObject , 'user') && $responseObject-> {'user'} == 'invalid')
+				$this->SetStatus(201);
+			if(property_exists($responseObject , 'token') && $responseObject-> {'token'} == 'invalid')
+				$this->SetStatus(202);
+			
+			IPS_LogMessage("Pushover", implode ( $responseObject-> {'errors'}, ' ; ') );
+            return false;
         }
 
         public function GlancesClear()
@@ -82,10 +92,26 @@
                 CURLOPT_SAFE_UPLOAD    => true,
                 CURLOPT_RETURNTRANSFER => true,
                 ]);
-            curl_exec($ch);
+			$response = curl_exec($ch);
             curl_close($ch);
-
-            return true;
+			
+			$responseObject = json_decode($response);
+			if ($responseObject == null) 
+			{
+				return false;
+			}
+			if($responseObject-> {'status'} == 1)
+			{
+				$this->SetStatus(102);
+				return true;
+			}			
+			if(property_exists($responseObject , 'user') && $responseObject-> {'user'} == 'invalid')
+				$this->SetStatus(201);
+			if(property_exists($responseObject , 'token') && $responseObject-> {'token'} == 'invalid')
+				$this->SetStatus(202);
+			
+			IPS_LogMessage("Pushover", implode ( $responseObject-> {'errors'}, ' ; ') );
+            return false;
         }
 
         public function Glances(string $title, string $text, string $subtext)
@@ -103,10 +129,26 @@
                 CURLOPT_SAFE_UPLOAD    => true,
                 CURLOPT_RETURNTRANSFER => true,
                 ]);
-            curl_exec($ch);
+			$response = curl_exec($ch);
             curl_close($ch);
-
-            return true;
+			
+			$responseObject = json_decode($response);
+			if ($responseObject == null) 
+			{
+				return false;
+			}
+			if($responseObject-> {'status'} == 1)
+			{
+				$this->SetStatus(102);
+				return true;
+			}			
+			if(property_exists($responseObject , 'user') && $responseObject-> {'user'} == 'invalid')
+				$this->SetStatus(201);
+			if(property_exists($responseObject , 'token') && $responseObject-> {'token'} == 'invalid')
+				$this->SetStatus(202);
+			
+			IPS_LogMessage("Pushover", implode ( $responseObject-> {'errors'}, ' ; ') );
+            return false;
         }
 
         public function GlancesCount(int $count)
@@ -123,10 +165,26 @@
                 CURLOPT_SAFE_UPLOAD    => true,
                 CURLOPT_RETURNTRANSFER => true,
                 ]);
-            curl_exec($ch);
+			$response = curl_exec($ch);
             curl_close($ch);
-
-            return true;
+			
+			$responseObject = json_decode($response);
+			if ($responseObject == null) 
+			{
+				return false;
+			}
+			if($responseObject-> {'status'} == 1)
+			{
+				$this->SetStatus(102);
+				return true;
+			}			
+			if(property_exists($responseObject , 'user') && $responseObject-> {'user'} == 'invalid')
+				$this->SetStatus(201);
+			if(property_exists($responseObject , 'token') && $responseObject-> {'token'} == 'invalid')
+				$this->SetStatus(202);
+			
+			IPS_LogMessage("Pushover", implode ( $responseObject-> {'errors'}, ' ; ') );
+            return false;
         }
 
         public function GlancesPercent(int $percent)
@@ -147,9 +205,25 @@
                 CURLOPT_SAFE_UPLOAD    => true,
                 CURLOPT_RETURNTRANSFER => true,
                 ]);
-            curl_exec($ch);
+			$response = curl_exec($ch);
             curl_close($ch);
-
-            return true;
+			
+			$responseObject = json_decode($response);
+			if ($responseObject == null) 
+			{
+				return false;
+			}
+			if($responseObject-> {'status'} == 1)
+			{
+				$this->SetStatus(102);
+				return true;
+			}			
+			if(property_exists($responseObject , 'user') && $responseObject-> {'user'} == 'invalid')
+				$this->SetStatus(201);
+			if(property_exists($responseObject , 'token') && $responseObject-> {'token'} == 'invalid')
+				$this->SetStatus(202);
+			
+			IPS_LogMessage("Pushover", implode ( $responseObject-> {'errors'}, ' ; ') );
+            return false;
         }
     }
